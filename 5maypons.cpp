8bc8af5ad23c56ad6dad767ef09fe8bcc0108470
bhavyashukla23 <65808829+bhavyashukla23@users.noreply.github.com>
2020-06-05 01:14:39 +0530

    5maypons
    a program to find product of two nos in cpp

---
+++ 5maypons.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float num1 , num2,prod;
+	cout << "Enter two numbers whose product is to be found out \n";
+	cin >> num1 >> num2;
+	prod=(num1 * num2);
+	cout << "The product of the two numbers entered is :" << prod;
+	return 0;
+}
